65d3b46b56371aa7d37e6315c502c41219402d25
AbinayaPandey <155822896+AbinayaPandey@users.noreply.github.com>
2025-09-20 08:27:30 +0545

    Update Download Audio.py
    I just change the code so that the user can input the url themself

--- Download Audio/Download Audio.py
+++ Download Audio/Download Audio.py
@@ -3,7 +3,8 @@ import pytube
 from moviepy.editor import *
 
 # Define the YouTube video URL
-youtube_url = "https://www.youtube.com/watch?v=E6eKvji_BoE"
+youtube_url = input("Enter the youtube url: ")
+# youtube_url = "https://www.youtube.com/watch?v=E6eKvji_BoE"
 
 # Create a PyTube object and get the audio stream
 yt = pytube.YouTube(youtube_url)
@@ -19,5 +20,6 @@ audio_clip.write_audiofile(mp3_file)
 
 # Clean up the temporary file
 os.remove(temp_file)
+
 
 print("Audio extracted and saved as MP3 file to", mp3_file)
